e81a02d9040e6625decba3b19a776839285831b5
Anderson Rubiano <anderrubiano@outlook.com>
2024-11-21 14:56:07 -0500

    issue/ add multiples themes for weather app

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './app/index.'
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app/index.";
+import "./index.css";
+import { ThemeProvider } from "./app/context/ThemeContext";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
-)
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
